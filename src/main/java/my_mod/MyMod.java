24539e9d3ce8d7cb64ca2ab5d20c9e306fec78ab
3TUSK <urey.s.knowledge@gmail.com>
2020-02-23 20:21:23 -0800

    Down the rabbit hole

---
+++ src/main/java/my_mod/MyMod.java
@@ -0,0 +1,18 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("examplemod")
+public class ExampleMod {
+
+    public ExampleMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        System.out.println("Hello, Forge");
+    }
+
+}
